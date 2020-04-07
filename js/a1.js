c77309747073221d282e2e21fba70c094233625b
fulishi <1678682397@qq.com>
2020-04-08 01:21:36 +0800

    上传网站源码到github

---
+++ js/a1.js
@@ -0,0 +1,23 @@
+/**
+ * Created by fulishi on 2020/3/21.
+ */
+//œ‘ æ±Í«©
+function showDIV(){
+    var element= document.getElementById("ph");
+    element.style.display="block";
+}
+//“˛≤ÿ±Í«©
+function hideDIV(){
+    var element = document.getElementById("ph");
+    element.style.display="none";
+}
+
+function showD(elementID){
+    var element=document.getElementById(elementID);
+    element.style.display="block";
+}
+
+function hideD(elementID){
+    var element=document.getElementById(elementID);
+    element.style.display="none";
+}
